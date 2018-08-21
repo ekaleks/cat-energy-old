d2747b0957ea440e00cdcd56b6013bfe67c4ca28
Алексеева Екатерина <klem4@yandex.ru>
2018-08-22 00:04:36 +0300

    Добавляет небольшие изменения

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,75 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-svgstore");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-cwebp");
+  grunt.loadNpmTasks("grunt-csso");
+  grunt.loadNpmTasks("grunt-juwain-posthtml");
 
   grunt.initConfig({
+    imagemin: {
+       images: {
+         options: {
+           optimizationLevel: 3,
+           progressive: true
+         },
+         files: [{
+           expand: true,
+           src: ["source/img/**/*.{png,jpg,svg}"]
+         }]
+       }
+    },
+
+    cwebp: {
+      images: {
+        options: {
+          q: 90
+        },
+        files: [{
+          expand: true,
+          src: ["source/img/**/*.{png,jpg}"]
+        }]
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "source/css/style.min.css": ["source/css/style.css"]
+        }
+      }
+    },
+
+    svgstore: {
+      options: {
+        includeTitleElement: false
+      },
+      sprite: {
+        files: {
+          "source/img/sprite.svg": ["source/img/icon-vk-mobile.svg","source/img/htmlacademy.svg","source/img/icon-insta.svg","source/img/icon-fb.svg"]
+        }
+      }
+    },
+
+    posthtml: {
+      options: {
+        use: [
+          require("posthtml-include")()
+        ]
+      },
+      html: {
+        files: [{
+
+          expand: true,
+          src: ["source/*.html"]
+        }]
+      }
+    },
+
     less: {
       style: {
         files: {
@@ -53,5 +120,11 @@ module.exports = function(grunt) {
     }
   });
 
+
+  grunt.registerTask("default", ["cwebp"]);
+  grunt.registerTask("default", ["csso"]);
+  grunt.registerTask("default", ["posthtml"]);
+  grunt.registerTask("default", ["svgstore"]);
+  grunt.registerTask("default", ["imagemin"]);
   grunt.registerTask("serve", ["browserSync", "watch"]);
 };
